49a0f6f13bec3cae303f6f86e4b3e7a16ce921f7
varun vijay <varunvijaymenon@gmail.com>
2024-07-07 19:20:57 +0530

    added naive two sum

---
+++ two_sum.py
@@ -0,0 +1,14 @@
+def two_sum(nums, target):
+
+
+    for i in range(0, len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i]+nums[j]==target:
+                return [i,j]
+            
+
+
+
+print(two_sum(nums = [2,7,11,15], target = 9))
+print(two_sum(nums = [3,2,4], target = 6))
+print(two_sum(nums = [3,3], target = 6))
